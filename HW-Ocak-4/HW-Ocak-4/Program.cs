08ae17518fc04074e002cc40bd05ecec5e9d52ac
aliemrepmk <aliemrepamuk0@gmail.com>
2025-03-12 14:59:50 +0300

    HW-Ocak-4

---
+++ HW-Ocak-4/HW-Ocak-4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Ali Emre Pamuk
+// HW-Ocak-4
+
+Console.WriteLine("1'den 10'a kadar sayılar: ");
+for(int i = 1; i <= 10; i++)
+{
+    Console.WriteLine(i);
+}
+
+Console.Write("\nBir sayı giriniz: ");
+int sayi = Convert.ToInt32(Console.ReadLine());
+int toplam = 0;
+
+
+for(int i = 1; i <= sayi; i++)
+{
+    toplam += i;
+}
+Console.WriteLine("1'den " + sayi + " kadar olan sayıların toplamı: " + toplam);
