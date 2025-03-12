36d2156de6f1c9a8c4f4de301e98bc3fec893932
aliemrepmk <aliemrepamuk0@gmail.com>
2025-03-12 14:52:59 +0300

    HW-Ocak-3

---
+++ HW-Ocak-3/HW-Ocak-3/Program.cs
@@ -0,0 +1,27 @@
+﻿// Ali Emre Pamuk
+// HW-Ocak-3
+
+Console.Write("Bir sayı girin: ");
+int sayi = Convert.ToInt32(Console.ReadLine());
+
+if (sayi % 2 == 0)
+{
+    Console.WriteLine("Girilen sayı çifttir.");
+}
+else
+{
+    Console.WriteLine("Girilen sayı tektir.");
+}
+
+if (sayi > 0)
+{
+    Console.WriteLine("Girilen sayı pozitiftir.");
+}
+else if (sayi < 0)
+{
+    Console.WriteLine("Girilen sayı negatiftir.");
+}
+else
+{
+    Console.WriteLine("Girilen sayı sıfırdır.");
+}
