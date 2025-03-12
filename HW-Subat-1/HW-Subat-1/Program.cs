a8fa76f568b9029ae68b4c293d394fcae98d0087
aliemrepmk <aliemrepamuk0@gmail.com>
2025-03-12 15:07:57 +0300

    HW-Subat-1

---
+++ HW-Subat-1/HW-Subat-1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Ali Emre Pamuk
+// HW-Şubat-1
+
+int[] dizi = new int[5];
+
+for (int i = 0; i < 5; i++)
+{
+    Console.Write("Dizinin " + (i + 1) + ". elemanını girin: ");
+    dizi[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+int enBuyuk = dizi[0];
+
+Console.WriteLine("\nGirilen dizinin elemanları:");
+foreach (int eleman in dizi)
+{
+    if (eleman > enBuyuk)
+    {
+        enBuyuk = eleman;
+    }
+    Console.WriteLine(eleman);
+}
+Console.WriteLine("\nDizinin en büyük elemanı: " + enBuyuk);
