4354db1b9104d32d8cc6a43a947930e6b238445f
aliemrepmk <aliemrepamuk0@gmail.com>
2025-03-12 14:48:31 +0300

    HW-Ocak-2

---
+++ HW-Ocak-2/HW-Ocak-2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Ali Emre Pamuk
+// HW-Ocak-2
+
+Console.Write("Birinci sayıyı girin: ");
+double sayi1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("İkinci sayıyı girin: ");
+double sayi2 = Convert.ToDouble(Console.ReadLine());
+
+double toplam = sayi1 + sayi2;
+
+Console.WriteLine("Toplam: " + toplam);
